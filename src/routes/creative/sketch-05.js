d7339d5567df422cec9ae39067aa0e68270772f1
Mike Peiman <mikepeiman@gmail.com>
2021-12-06 09:45:27 -0500

    drawing glyph on new canvas/context, replacing grid cells with drawing

--- src/routes/creative/sketch-05.js
+++ src/routes/creative/sketch-05.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
-
+const math = require('canvas-sketch-util/math')
+const random = require('canvas-sketch-util/random')
 let manager
 let fontFamily = "Century"
 let width = height = 1080
@@ -7,39 +8,76 @@ const settings = {
   dimensions: [width, height]
 };
 
-let text = "m"
+let text = "A"
 let fontSize = 1200
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, width, height);
-    const pen = context
+const typeCanvas = document.createElement('canvas')
+const typeContext = typeCanvas.getContext('2d')
 
-    pen.fillStyle = 'white'
-    pen.font = `${fontSize}px ${fontFamily}`
-    pen.textBaseline = 'top'
-    // pen.textAlign = 'center'
-    // text = "M"
-    let metrics = pen.measureText(text)
-    console.log(`🚀 ~ file: sketch-05.js ~ line 20 ~ return ~ metrics `, metrics)
+
+const sketch = ({ context, width, height }) => {
+  const cell = 20
+  const cols = Math.floor(width / cell)
+  const rows = Math.floor(height / cell)
+  const numCells = cols * rows
+  typeCanvas.width = cols
+  typeCanvas.height = rows
+
+
+  return ({ context, width, height }) => {
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, cols, rows);
+    const pen = context
+    fontSize = cols
+    typeContext.fillStyle = 'white'
+    typeContext.font = `${fontSize}px ${fontFamily}`
+    typeContext.textBaseline = 'top'
+    let metrics = typeContext.measureText(text)
+    // console.log(`🚀 ~ file: sketch-05.js ~ line 20 ~ return ~ metrics `, metrics)
     let mx = metrics.actualBoundingBoxLeft * -1
     let my = metrics.actualBoundingBoxAscent * -1
     let mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
     let mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent
-    let x = (width - mw) * .5 - mx
-    let y = (height - mh) * .5 - my
+    let tx = (cols - mw) * .5 - mx
+    let ty = (rows - mh) * .5 - my
 
-    pen.save()
-    pen.translate(x, y)
-    pen.beginPath()
-    pen.rect(mx, my, mw, mh)
-    pen.strokeStyle = "#aabbff"
-    pen.stroke()
-    pen.fillText(text, 0, 0)
-    pen.restore()
+    typeContext.save()
+    typeContext.translate(tx, ty)
+    typeContext.beginPath()
+    // typeContext.rect(mx, my, mw, mh)
+    // typeContext.strokeStyle = "#aabbff"
+    // typeContext.stroke()
+    typeContext.fillText(text, 0, 0)
+    typeContext.restore()
 
+    let typeData = typeContext.getImageData(0, 0, cols, rows).data
+    // console.log(`🚀 ~ file: sketch-05.js ~ line 53 ~ return ~ typeData`, typeData)
 
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols
+      const row = Math.floor(i/ cols)
+      const x = col * cell
+      const y = row * cell
+
+      const r = typeData[i * 4 + 0]
+      const g = typeData[i * 4 + 1]
+      const b = typeData[i * 4 + 2]
+      const a = typeData[i * 4 + 3]
+
+      // pen.fillStyle = randomRGBA()
+      pen.fillStyle = `rgba(${r},${g},${b},${a})`
+      pen.save()
+      pen.translate(x, y)
+      pen.translate(cell * .5, cell * .5)
+
+      // pen.fillRect(0,0, cell, cell)
+      pen.beginPath()
+      pen.arc(0,0, cell / 2, 0, Math.PI * 2)
+      pen.fill()
+      pen.restore()
+    }
+
+    pen.drawImage(typeCanvas, 0, 0)
 
   };
 };
@@ -56,6 +94,13 @@ const start =  async () => {
 }
 
 start()
+
+
+const randomRGBA = (r = random.range(0, 255), g  = random.range(0, 255), b = random.range(0, 255), a = random.range(0.1, 1)) => {
+  let color = `hsla(${r},${g}%,${b}%,${a})`
+  return color
+}
+
 
 /*
 
