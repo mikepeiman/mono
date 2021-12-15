acbaac3cb55669b1584d74e09369657d7555ad39
Mike Peiman <mikepeiman@gmail.com>
2021-12-14 20:15:49 -0500

    added adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import preprocess from 'svelte-preprocess';
 import path from 'path'
 import { normalizePath } from 'vite'
+import vercel from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	root: normalizePath(path.resolve('./')),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: vercel()
 		files: {
 			assets: 'static'
 		},
