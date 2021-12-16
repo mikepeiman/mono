424c04956e8e670b955b886bfb00c3c66dd12e57
Mike Peiman <mikepeiman@gmail.com>
2021-12-15 20:30:47 -0500

    renamed and moved reference backup upload/.json.js files which caused Vercel build error

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,75 +1,75 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    mode: 'jit',
-    content: ['./src/**/*.svelte'],
-    theme: {
-        extend: {
-            colors: {
-                ...colors,
-            }
-        }
+  mode: 'jit',
+  content: ['./src/**/*.svelte'],
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+      }
+    }
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = '') {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+
+          const newVars =
+            typeof value === 'string'
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ':root': extractColorVars(theme('colors')),
+      });
     },
-    variants: {},
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('daisyui'),
-        function({ addBase, theme }) {
-          function extractColorVars(colorObj, colorGroup = '') {
-            return Object.keys(colorObj).reduce((vars, colorKey) => {
-              const value = colorObj[colorKey];
-    
-              const newVars =
-                typeof value === 'string'
-                  ? { [`--color${colorGroup}-${colorKey}`]: value }
-                  : extractColorVars(value, `-${colorKey}`);
-    
-              return { ...vars, ...newVars };
-            }, {});
-          }
-    
-          addBase({
-            ':root': extractColorVars(theme('colors')),
-          });
+  ],
+  daisyui: {
+    themes: [
+      {
+        'quotes': {                          /* your theme name */
+          'primary': '#a991f7',           /* Primary color */
+          'primary-focus': '#8462f4',     /* Primary color - focused */
+          'primary-content': '#ffffff',   /* Foreground content color to use on primary color */
+
+          'secondary': '#f6d860',         /* Secondary color */
+          'secondary-focus': '#f3cc30',   /* Secondary color - focused */
+          'secondary-content': '#ffffff', /* Foreground content color to use on secondary color */
+
+          'accent': '#37cdbe',            /* Accent color */
+          'accent-focus': '#2aa79b',      /* Accent color - focused */
+          'accent-content': '#ffffff',    /* Foreground content color to use on accent color */
+
+          'neutral': '#3d4451',           /* Neutral color */
+          'neutral-focus': '#2a2e37',     /* Neutral color - focused */
+          'neutral-content': '#ffffff',   /* Foreground content color to use on neutral color */
+
+          'base-100': '#112233',          /* Base color of page, used for blank backgrounds */
+          'base-200': '#051122',          /* Base color, a little darker */
+          'base-300': '#020511',          /* Base color, even more darker */
+          'base-content': '#cfeff7',      /* Foreground content color to use on base color */
+
+          'info': '#2094f3',              /* Info */
+          'success': '#009485',           /* Success */
+          'warning': '#ff9900',           /* Warning */
+          'error': '#ff5724',             /* Error */
         },
-      ],
-    daisyui: {
-        themes: [
-          {
-            'quotes': {                          /* your theme name */
-               'primary' : '#a991f7',           /* Primary color */
-               'primary-focus' : '#8462f4',     /* Primary color - focused */
-               'primary-content' : '#ffffff',   /* Foreground content color to use on primary color */
-    
-               'secondary' : '#f6d860',         /* Secondary color */
-               'secondary-focus' : '#f3cc30',   /* Secondary color - focused */
-               'secondary-content' : '#ffffff', /* Foreground content color to use on secondary color */
-    
-               'accent' : '#37cdbe',            /* Accent color */
-               'accent-focus' : '#2aa79b',      /* Accent color - focused */
-               'accent-content' : '#ffffff',    /* Foreground content color to use on accent color */
-    
-               'neutral' : '#3d4451',           /* Neutral color */
-               'neutral-focus' : '#2a2e37',     /* Neutral color - focused */
-               'neutral-content' : '#ffffff',   /* Foreground content color to use on neutral color */
-    
-               'base-100' : '#112233',          /* Base color of page, used for blank backgrounds */
-               'base-200' : '#051122',          /* Base color, a little darker */
-               'base-300' : '#020511',          /* Base color, even more darker */
-               'base-content' : '#cfeff7',      /* Foreground content color to use on base color */
-    
-               'info' : '#2094f3',              /* Info */
-               'success' : '#009485',           /* Success */
-               'warning' : '#ff9900',           /* Warning */
-               'error' : '#ff5724',             /* Error */
-            },
-          },
-          'dark',
-          'forest',
-          'aqua',
-          'fantasy',
-          'luxury',
-          'CMYK'
-        ],
+      },
+      'dark',
+      'forest',
+      'aqua',
+      'fantasy',
+      'luxury',
+      'CMYK'
+    ],
   }
 }
