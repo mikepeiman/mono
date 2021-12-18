8d1aa55e6afecde232a03400ab37fde2476bf526
Mike Peiman <mikepeiman@gmail.com>
2021-12-18 10:02:53 -0500

    boilerplate content and styling progress

--- src/routes/mikepeiman/index.svelte
+++ src/routes/mikepeiman/index.svelte
@@ -10,9 +10,10 @@
 </script>
 
 <script>
+	import Time from 'svelte-time';
 	import Posts from '$components/Posts.svelte';
 	export let posts;
-    let selectedPosts = posts?.slice(0,3)
+	let selectedPosts = posts?.slice(0, 3);
 	console.log(`🚀 ~ file: index.svelte ~ line 4 ~ posts`, posts);
 	let projects = [
 		{
@@ -34,34 +35,49 @@
 </script>
 
 <main class="center">
-	<section class="articles">
-		<div class="posts py-12">
+	<section class="articles flex flex-row">
+		<div class="intro py-12 m-3 w-1/2 prose prose-lg">
+			<h1>Hi, I'm Mike.</h1>
+			<p>I'm a creative and visionary software developer, and this is my professional blog.</p>
+            <ol>
+                <li>This is one.</li>
+                <li>This is two.</li>
+                <li>This is three.</li>
+            </ol>
+		</div>
+		<div class="posts py-12 w-1/2">
 			{#each selectedPosts as post}
-                 <div class="card shadow card-body">
-                     {post.date}
-                     {post.title}
-                    
-                    
+				<div class="card shadow-sm shadow-sky-200 m-3 card-body prose">
+					<div class="p-0 m-0">
+                        <Time timestamp={post.date} />
                     </div>
-            {/each}
-		</div>
-		<div class="text-grey-200 text-2xl bold italic py-3">
-			Truth, freedom, health, wisdom, goodness: I'm dedicated to these things, and actively trying
-			to contribute to a society that embodies them more fully.
+					<p ckass="text-xl">{post.title}</p>
+					<p class="m-0">{post.excerpt}</p>
+				</div>
+			{/each}
 		</div>
 
-		<section class="tiles panel flex">
-			<h2 class="uppercase">Selected Projects</h2>
+	</section>
+    <section class="flex m-6">
+        <div class="text-grey-200 text-2xl bold italic prose">
+            Truth, freedom, health, wisdom, goodness: I'm dedicated to these things, and actively trying
+            to contribute to a society that embodies them more fully.
+        </div>
+    </section>
+	<section class="projects">
+		<h2 class="uppercase">Selected Projects</h2>
+		<div class="card card-shadow flex flex-row">
 			{#each projects as project}
-				<div class="card p-4 bg-teal-900 m-3 rounded-md">
-					{#each project.tags as tag}
-						<span>{tag}</span>
-					{/each}
-
-					<h2>{project.name}</h2>
+				<div class="card p-6 bg-teal-900 m-3 rounded-md prose">
+					<div class="flex flex-row">
+                        {#each project.tags as tag}
+                            <span class="mx-2 bg-slate-900 rounded-md py-1 px-2">{tag}</span>
+                        {/each}
+                    </div>
+					<h2 class="my-6">{project.name}</h2>
 					<p>{project.desc}</p>
 				</div>
 			{/each}
-		</section>
+		</div>
 	</section>
 </main>
