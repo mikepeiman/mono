d9b4f40cb58535b9357b7ea595ca839a9d1d0115
Mike Peiman <mikepeiman@gmail.com>
2021-12-24 10:17:58 -0500

    building basic layout - good course, fast-moving

--- src/routes/egghead-discord/index.svelte
+++ src/routes/egghead-discord/index.svelte
@@ -1,44 +1,19 @@
 <script>
-	
 </script>
 
 <!-- Beginning to work through https://egghead.io/lessons/tailwind-intro-to-styling-custom-uis-with-tailwind-utility-classes -->
-<div
-	class="bg-electricindigo-500 bg-opacity-10 min-h-full min-w-full flex items-center justify-center"
->
-	<div class="max-w-lg">
-		<!-- <img src="https://via.placeholder.com/150.jpg" alt=""> -->
-		<div class="flex hover:bg-gray-700 hover:bg-opacity-30 px-4 py-1">
-			<img class="w-10 h-10 rounded-full mr-4" src="https://loremflickr.com/100/100" alt="" />
-			<div>
-				<p class="flex items-baseline">
-					<span class="text-electricindigo-400 mr-2 text-sm font-medium">mikepeiman</span>
-					<span class="text-sm text-gray-500">12/24/2021</span>
-				</p>
-				<p class="text-gray-300">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates hic cupiditate
-					accusantium eos tempora fuga et eveniet explicabo iure cum voluptas, rem non expedita
-					cumque atque esse nulla fugiat sunt nostrum vel reprehenderit sit quae corporis quo? Minus
-					laboriosam placeat assumenda veniam est debitis dolor sapiente voluptate, aut, corporis
-					magnam.
-				</p>
-			</div>
-		</div>
-		<div class="flex mt-2 hover:bg-gray-700 hover:bg-opacity-30 px-4 py-1">
-			<p class="text-gray-300 pl-14">
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam distinctio, sed ex animi
-				neque laborum qui consectetur vitae atque? Soluta pariatur omnis, quidem praesentium laborum
-				quasi rem expedita est at.
-			</p>
-		</div>
-		<div class="flex mt-2 hover:bg-gray-700 hover:bg-opacity-30 px-4 py-1">
-			<p class="text-gray-300 pl-14">
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam distinctio, sed ex animi
-				neque laborum qui consectetur vitae atque? Soluta pariatur omnis, quidem praesentium laborum
-				quasi rem expedita est at.
-			</p>
+
+<div class="flex flex-row text-white h-screen w-full">
+	<div class="bg-gray-800 p-4">
+		<div class="bg-white text-gray-800 w-12 h-12 rounded-full flex items-center justify-center">
+			MP
 		</div>
 	</div>
+	<div class="bg-gray-700 w-60 flex flex-col">
+		<div class="sticky  p-4">Header</div>
+		<div class="bg-blue-900 p-4 flex-1">Channels</div>
+	</div>
+	<div class="bg-gray-600 flex-1 p-4">Main</div>
 </div>
 
 <style lang="scss">
