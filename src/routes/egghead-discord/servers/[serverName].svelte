bd505c6de02b7218cbc76a07e039ad3a5a767fa2
Mike Peiman <mikepeiman@gmail.com>
2021-12-26 16:49:32 -0500

    reviewed and implemented two basic methods to get page param for dynamic route

---
+++ src/routes/egghead-discord/servers/[serverName].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+    // export async function load({page, fetch, session, context}) {
+    //     return {
+    //         props: {
+    //             serverName: page.params.serverName
+    //         }
+    //     }
+    // }
+</script>
+
+<script>
+    // export let serverName
+    import { page }from '$app/stores'
+    const serverName = $page.params.serverName
+</script>
+
+<h1>{serverName}</h1>
