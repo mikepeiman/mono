20f636d2097c945f5266dca1945466c290478e5b
Mike Peiman <mikepeiman@gmail.com>
2021-12-27 11:42:46 -0500

    fine-tuned styling for hover and active server symbols

--- src/routes/egghead-discord/Server.svelte
+++ src/routes/egghead-discord/Server.svelte
@@ -5,18 +5,20 @@
 
 <a href="/egghead-discord/{serverId}" class="relative space-y-2 block group">
 	<div class="absolute -left-3 h-full flex items-center">
-		<div class="bg-white w-1 h-5 rounded-r opacity-0 group-hover:opacity-100 transition-all duration-200
+		<div class="
 		{path === `/egghead-discord/${serverId}` 
-		? '' 
-		: ''} 
-		" ></div>
+		? 'h-10' 
+		: 'h-5 opacity-0 scale-0 origin-left group-hover:scale-100 group-hover:opacity-100'} 
+		 transition-all duration-200 bg-white w-1  rounded-r" ></div>
 	</div>
-	<div
-		class=" w-12 h-12 flex items-center justify-center 
-		{path === `/egghead-discord/${serverId}`
-			? 'bg-blue-500 text-white rounded-2xl'
-			: 'text-gray-100 rounded-3xl bg-gray-600 group-hover:bg-blue-500 group-hover:text-white transition-all duration-200 rounded-3xl group-hover:rounded-2xl'}
-			">
-		{serverId}
+	<div class="group-active:translate-y-[1px]">
+		<div
+			class=" w-12 h-12 flex items-center justify-center
+			{path === `/egghead-discord/${serverId}`
+				? 'bg-blue-500 text-white rounded-2xl'
+				: 'text-gray-100 rounded-3xl bg-gray-600 group-hover:bg-blue-500 group-hover:text-white transition-all duration-200 rounded-3xl group-hover:rounded-2xl'}
+				">
+			{serverId}
+		</div>
 	</div>
 </a>
