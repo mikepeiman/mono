25bc746b002ae607f762c10bd5509d21093a4207
Mike Peiman <mikepeiman@gmail.com>
2021-12-29 09:09:37 -0500

    continued styling, icons and hover

--- src/routes/egghead-discord/Channel.svelte
+++ src/routes/egghead-discord/Channel.svelte
@@ -20,7 +20,7 @@
         {path === `/egghead-discord/${serverId}/${channelId}`
 				? 'bg-sky-600/[0.32]'
 				: 'group-hover:bg-sky-800/[0.16]'} 
-         transition-all duration-200 bg-gray-800 text-white rounded-r relative px-2 ml-2 rounded-sm flex align-center group-active:bg-sky-800/[0.32]"
+          bg-gray-800 bg-opacity-0 text-white rounded-r relative px-2 ml-2 rounded-sm flex align-center group-active:bg-sky-800/[0.32]"
 		>
 			<a
 				href="/egghead-discord/{serverId}/{channelId}"
@@ -34,15 +34,16 @@
 				</div>
 				<div class="flex align-center" aria-hidden="true">
 					<IconHashSmall class="mt-1 font-thin text-gray-500" />
-					<div class="text-gray-300">{channelId}</div>
-					<div class="
+					<div class="text-gray-300 pl-2">{channelId}</div>
+					<div
+						class="
                     {path === `/egghead-discord/${serverId}/${channelId}`
 							? 'opacity-1'
 							: 'opacity-0 group-hover:opacity-50'} 
-                    text-fuchsia-300 flex align-center absolute top-0 right-0 mt-[12px] mr-2 text-[10px]"
+                    flex align-center absolute top-0 right-0 mt-[10px] mr-2"
 					>
 						<IconPersonPlus
-							class="text-blue-300 opacity-1  group-hover:opacity-1 transition-all"
+							class="text-blue-300 opacity-1 w-4 h-4 group-hover:opacity-1 transition-all"
 						/>
 					</div>
 				</div></a
--- src/routes/egghead-discord/Channels.svelte
+++ src/routes/egghead-discord/Channels.svelte
@@ -2,7 +2,10 @@
 	import Channel from './Channel.svelte';
 	import IconBadgeCheck from '~icons/bx/bxs-badge-check';
 	import IconChevronDown from '~icons/mdi/chevron-down';
-
+	import IconPlaylistCheck from '~icons/gg/play-list-check'; //gg:play-list-check
+	import IconBookBookmark from '~icons/bx/bx-book-bookmark';
+	import IconBullhorn from '~icons/whh/bullhorn';
+    import IconPersonPlus from '~icons/bi/person-plus-fill';
 	export let servers, serverIndex;
 
 	let server;
@@ -11,9 +14,7 @@
 </script>
 
 <div class="bg-gray-800  shadow-sm z-10 shadow-gray-900 hover:bg-gray-700/[0.16]">
-	<div
-		class="px-3 h-12 flex flex-shrink-0 items-center font-fira "
-	>
+	<div class="px-3 h-12 flex flex-shrink-0 items-center font-fira ">
 		<div class="icon-badge-check text-gray-600 relative flex items-center">
 			<div class="bg-white absolute w-3 h-3 rounded-xl -z-10 left-[3px]" />
 			<div class="mr-[2px]">
@@ -27,10 +28,30 @@
 	</div>
 </div>
 <div class="flex flex-col overflow-y-scroll h-full">
+	<div class="header-icons mt-3">
+		<div class="flex group relative text-gray-500 items-center py-[6px] px-2 ml-2  hover:bg-gray-500/[0.16] hover:rounded-l">
+			<IconBookBookmark class="w-5 h-5" />
+			<div class="pl-2 font-bold font-cairo">welcome</div>
+            <div class="opacity-0 group-hover:opacity-50 flex align-center absolute top-0 right-0 mt-[10px] mr-2" >
+                <IconPersonPlus
+                class="text-blue-300 opacity-1 w-4 h-4"
+                        />
+            </div>
+		</div>
+		<div class="flex group relative text-gray-500 items-center py-[6px] px-2 ml-2  hover:bg-gray-500/[0.16] hover:rounded-l">
+			<IconBullhorn class="w-5 h-5" />
+			<div class="pl-2 font-bold font-cairo">announcements</div>
+            <div class="opacity-0 group-hover:opacity-50 flex align-center absolute top-0 right-0 mt-[10px] mr-2" >
+                <IconPersonPlus
+                class="text-blue-300 opacity-1 w-4 h-4"
+                        />
+            </div>
+		</div>
+
+	</div>
 	<!-- {#if channels} -->
 	{#each servers[serverIndex].channels as channel}
 		<!-- content here -->
-
 		<Channel serverId={servers[serverIndex].id} channelId={channel.id} />
 		<!-- <div class="bg-blue-900 p-3 flex-1">{channel.id}</div> -->
 	{/each}
